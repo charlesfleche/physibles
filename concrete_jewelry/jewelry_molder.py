66d683387e1dd7d5404d15ed982d4eafb2fb0821
Charles Flèche <charles.fleche@ubisoft.com>
2026-03-13 13:31:35 -0400

    Add shapes

--- concrete_jewelry/jewelry_molder.py
+++ concrete_jewelry/jewelry_molder.py
@@ -12,46 +12,88 @@ attach_hole_radius = attach_hole_diameter
 width = 30.0 * MM
 thickness = min_concrete_size * 3
 
+parts = []
+
+# Disk
+
 with BuildPart() as ex3:
-    part = ex3
-    # part += [ex3]
     with BuildSketch() as ex3_sk:
         radius = width / 2.0
         Circle(width/2.0, align=Align.CENTER)
         with Locations((0, radius - min_concrete_size - attach_hole_radius)):
             Circle(attach_hole_radius, mode=Mode.SUBTRACT)
-        # Rectangle(length, width, mode=Mode.SUBTRACT)
-    # part = extrude(amount=thickness)
-    part = extrude(amount=thickness)
+    extrude(amount=thickness)
+    parts.append(ex3.part)
 
-bbox = part.bounding_box()
-silicon_mold = Box(
-    bbox.max.X - bbox.min.X + 2 * mold_wall_size,
-    bbox.max.Y - bbox.min.Y + 2 * mold_wall_size,
-    bbox.max.Z  + mold_wall_size,
-    align=(Align.CENTER, Align.CENTER, Align.MIN)
-)
-silicon_mold -= part
 
-bbox = silicon_mold.bounding_box()
-mold = Box(
-    bbox.max.X - bbox.min.X + 2 * mold_wall_size,
-    bbox.max.Y - bbox.min.Y + 2 * mold_wall_size,
-    bbox.max.Z + mold_base_size,
-    align=(Align.CENTER, Align.CENTER, Align.MIN)
-)
-mold -= silicon_mold
+# Square straight
 
-z_pack = pack(
-    [
-        part,
-        silicon_mold,
-        mold
-    ],
-    padding=5,
-    align_z=True
-)
+with BuildPart() as ex3:
+    with BuildSketch() as ex3_sk:
+        radius = width / 2.0
+        Rectangle(width, width, align=Align.CENTER)
+        with Locations((0, radius - min_concrete_size - attach_hole_radius)):
+            Circle(attach_hole_radius, mode=Mode.SUBTRACT)
+    extrude(amount=thickness)
+    parts.append(ex3.part)
+
+
+# Square up straight
+
+with BuildPart() as ex3:
+    with BuildSketch() as ex3_sk:
+        with Locations(Rotation(0, 0, 45)):
+            Rectangle(width, width, align=Align.CENTER)
+        with Locations((0, radius - min_concrete_size - attach_hole_radius)):
+            Circle(attach_hole_radius, mode=Mode.SUBTRACT)
+    extrude(amount=thickness)
+    parts.append(ex3.part)
+
+
+# Triangle
+
+with BuildPart() as ex3:
+    with BuildSketch() as ex3_sk:
+        with Locations(Rotation(0, 0, 180)):
+            Triangle(a=width, b=width, c=width, align=Align.CENTER)
+        with Locations((0, radius - min_concrete_size - attach_hole_radius)):
+            Circle(attach_hole_radius, mode=Mode.SUBTRACT)
+    extrude(amount=thickness)
+    parts.append(ex3.part)
+
+
+z_pack = pack(parts, padding=mold_wall_size, align_z=True)
 show(z_pack)
+# show_object(ex3_sk)
+
+# bbox = part.bounding_box()
+# silicon_mold = Box(
+#     bbox.max.X - bbox.min.X + 2 * mold_wall_size,
+#     bbox.max.Y - bbox.min.Y + 2 * mold_wall_size,
+#     bbox.max.Z  + mold_wall_size,
+#     align=(Align.CENTER, Align.CENTER, Align.MIN)
+# )
+# silicon_mold -= part
+
+# bbox = silicon_mold.bounding_box()
+# mold = Box(
+#     bbox.max.X - bbox.min.X + 2 * mold_wall_size,
+#     bbox.max.Y - bbox.min.Y + 2 * mold_wall_size,
+#     bbox.max.Z + mold_base_size,
+#     align=(Align.CENTER, Align.CENTER, Align.MIN)
+# )
+# mold -= silicon_mold
+
+# z_pack = pack(
+#     [
+#         part,
+#         silicon_mold,
+#         mold
+#     ],
+#     padding=5,
+#     align_z=True
+# )
+# show(z_pack)
 # show_object(part)
 # show_object(silicon_mold)
 # show_object(mold)
